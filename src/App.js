c4f3042b304847b1de9d717b9c1a192613c6deaf
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-03 11:23:06 +0530

    Added appointment data and appointment component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import Search from "./components/Search";
-import { BiArchive } from "react-icons/bi"
+import AddAppointment from "./components/AddAppointment";
+import appointmentList from "./data.json"
+import { BiCalendar,BiTrash } from "react-icons/bi"
+import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-      <BiArchive className="inline-block text-red-400 align-top" />Your Appointments</h1>
+      <h1 className="text-5xl mb-3">
+      <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
+        <AddAppointment/>
         <Search/>
+
+        <ul className="divide-y divide-gray-200 ">
+          {
+            appointmentList.map(appointment=>(
+              <AppointmentInfo key={appointment.id} petName={appointment.petName} aptDate={appointment.aptDate} ownerName={appointment.ownerName} aptNotes={appointment.aptNotes}/>
+            ))
+          }
+        </ul>
     </div>
   );
 }
