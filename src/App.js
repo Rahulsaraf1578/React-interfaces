959bf75261b2cc05474a4151efb2572b202d0293
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-04 11:45:34 +0530

    Completed the project

--- src/App.js
+++ src/App.js
@@ -45,7 +45,10 @@ function App() {
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
-      <AddAppointment />
+      <AddAppointment 
+        onSendAppointment={myAppointment=>setAppointmentList([...appointmentList,myAppointment])}
+        lastId={appointmentList.reduce((max,item)=>Number(item.id)>max?Number(item.id):max,0)}
+      />
       <Search query={query} 
       // Recieve something in myQuery and then pass it in setQuery
       onQueryChange={myQuery => setQuery(myQuery)}
